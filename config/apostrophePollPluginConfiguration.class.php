d0819a20168a99e63eda1bd8442adc05be91539f
Raffaele Bolliger <raffaele.bolliger@gmail.com>
2012-03-26 22:51:17 +0200

    Generated new apostropePoll plugin

---
+++ config/apostrophePollPluginConfiguration.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * apostrophePollPlugin configuration.
+ * 
+ * @package     apostrophePollPlugin
+ * @subpackage  config
+ * @author      Buddies SARL
+ * @version     SVN: $Id: PluginConfiguration.class.php 17207 2009-04-10 15:36:26Z Kris.Wallsmith $
+ */
+class apostrophePollPluginConfiguration extends sfPluginConfiguration
+{
+  const VERSION = '1.0.0-DEV';
+
+  /**
+   * @see sfPluginConfiguration
+   */
+  public function initialize()
+  {
+  }
+}

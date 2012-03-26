be0d73d8b92bfdc22219bb6632169f20e8b3026e
Raffaele Bolliger <raffaele.bolliger@gmail.com>
2012-03-26 23:14:07 +0200

    Generated new aPoll slot type and enabled it in frontend.

---
+++ modules/aPollSlot/actions/components.class.php
@@ -0,0 +1,21 @@
+<?php
+class aPollSlotComponents extends aSlotComponents
+{
+  public function executeEditView()
+  {
+    // Must be at the start of both view components
+    $this->setup();
+    
+    // Careful, don't clobber a form object provided to us with validation errors
+    // from an earlier pass
+    if (!isset($this->form))
+    {
+      $this->form = new aPollSlotEditForm($this->id, $this->slot->getArrayValue());
+    }
+  }
+  public function executeNormalView()
+  {
+    $this->setup();
+    $this->values = $this->slot->getArrayValue();
+  }
+}

2c972ec51977f91ebc9d1b38c71da22bed559bb4
Raffaele Bolliger <raffaele.bolliger@gmail.com>
2012-03-27 23:47:05 +0200

    - Customized form to display in editView - Added translation catalogue

--- lib/form/aPollSlotEditForm.class.php
+++ lib/form/aPollSlotEditForm.class.php
@@ -1,25 +1,35 @@
-<?php    
-class aPollSlotEditForm extends BaseForm
-{
-  // Ensures unique IDs throughout the page
-  protected $id;
-  public function __construct($id, $defaults = array(), $options = array(), $CSRFSecret = null)
-  {
-    $this->id = $id;
-    parent::__construct($defaults, $options, $CSRFSecret);
-  }
-  public function configure()
-  {
-    // ADD YOUR FIELDS HERE
-    
-    // A simple example: a slot with a single 'text' field with a maximum length of 100 characters
-    $this->setWidgets(array('text' => new sfWidgetFormTextarea()));
-    $this->setValidators(array('text' => new sfValidatorString(array('required' => false, 'max_length' => 100))));
-    
-    // Ensures unique IDs throughout the page. Hyphen between slot and form to please our CSS
-    $this->widgetSchema->setNameFormat('slot-form-' . $this->id . '[%s]');
-    
-    // You don't have to use our form formatter, but it makes things nice
-    $this->widgetSchema->setFormFormatterName('aAdmin');
-  }
+<?php
+
+class aPollSlotEditForm extends BaseForm {
+
+    // Ensures unique IDs throughout the page
+    protected $id;
+
+    public function __construct($id, $defaults = array(), $options = array(), $CSRFSecret = null) {
+        $this->id = $id;
+        parent::__construct($defaults, $options, $CSRFSecret);
+    }
+
+    public function configure() {
+
+        $this->setWidgets(array('poll' => new sfWidgetFormDoctrineChoice(array(
+                'model' => 'aPollPoll',
+                'label' => 'Poll',
+                
+            ))));
+        $this->setValidators(array('poll' => new sfValidatorDoctrineChoice(array(
+                'model' => 'aPollPoll',
+            ))));
+
+        // Ensures unique IDs throughout the page. Hyphen between slot and form to please our CSS
+        $this->widgetSchema->setNameFormat('slot-form-' . $this->id . '[%s]');
+
+        // You don't have to use our form formatter, but it makes things nice
+        $this->widgetSchema->setFormFormatterName('aAdmin');
+        
+        
+        // setting translation catalogue
+        $this->widgetSchema->getFormFormatter()->setTranslationCatalogue('apoll');
+    }
+
 }
--- modules/aPollSlot/templates/_editView.php
+++ modules/aPollSlot/templates/_editView.php
@@ -1,2 +1,6 @@
 <?php // Just echo the form. You might want to render the form fields differently ?>
 <?php echo $form ?>
+
+<span class="a-slot-form-help">
+<?php echo __('Select a poll from the list. You can define new poll using the administration module.', null, 'apoll'); ?>
+</span>

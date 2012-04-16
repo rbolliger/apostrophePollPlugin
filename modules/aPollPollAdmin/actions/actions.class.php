0125edcbb2a5a41918d50c2ca80662bf8636ee6b
Raffaele Bolliger <raffaele.bolliger@gmail.com>
2012-04-16 13:20:47 +0200

    Moved code from action to base

--- modules/aPollPollAdmin/actions/actions.class.php
+++ modules/aPollPollAdmin/actions/actions.class.php
@@ -11,32 +11,6 @@ require_once dirname(__FILE__) . '/../lib/BaseaPollPollAdminActions.class.php';
  */
 class aPollPollAdminActions extends BaseaPollPollAdminActions {
 
-    public function executePreviewPoll(sfWebRequest $request) {
-
-
-        $this->poll_values = $request->getParameter('a_poll_poll');
-
-        $name = $this->poll_values['type'];
-
-        $this->poll_validation = aPollToolkit::checkPollConfiguration($name);
-
-        $culture = $this->getUser()->getCulture();
-
-        // filling data required to preview the poll
-        $this->poll = new aPollPoll();
-        $this->poll->setTitle($this->poll_values[$culture]['title']);
-        $this->poll->setDescription($this->poll_values[$culture]['description']);
-
-        $form = aPollToolkit::getPollFormName($name);
-        $this->form = new $form();
-
-        $this->form_view_template = aPollToolkit::getPollViewTemplate($name);
-        $this->submit_action = aPollToolkit::getPollSubmitAction($name);
-
-        // some fake values, just to call the form partial
-        $this->pageid = 1;
-        $this->name = 'dfsd';
-        $this->permid = 1;
-    }
+    
 
 }
--- modules/aPollPollAdmin/lib/BaseaPollPollAdminActions.class.php
+++ modules/aPollPollAdmin/lib/BaseaPollPollAdminActions.class.php
@@ -55,5 +55,33 @@ abstract class BaseaPollPollAdminActions extends autoaPollPollAdminActions
     $this->setTemplate('new');
     
   }
+  
+  public function executePreviewPoll(sfWebRequest $request) {
+
+
+        $this->poll_values = $request->getParameter('a_poll_poll');
+
+        $name = $this->poll_values['type'];
+
+        $this->poll_validation = aPollToolkit::checkPollConfiguration($name);
+
+        $culture = $this->getUser()->getCulture();
+
+        // filling data required to preview the poll
+        $this->poll = new aPollPoll();
+        $this->poll->setTitle($this->poll_values[$culture]['title']);
+        $this->poll->setDescription($this->poll_values[$culture]['description']);
+
+        $form = aPollToolkit::getPollFormName($name);
+        $this->form = new $form();
+
+        $this->form_view_template = aPollToolkit::getPollViewTemplate($name);
+        $this->submit_action = aPollToolkit::getPollSubmitAction($name);
+
+        // some fake values, just to call the form partial
+        $this->pageid = 1;
+        $this->name = 'dfsd';
+        $this->permid = 1;
+    }
     
 }

699ad6f22ed07464d31ec4c41f0d4233380a947a
Raffaele Bolliger <raffaele.bolliger@gmail.com>
2012-04-20 15:37:34 +0200

    Added link to show ansswer in email body

--- modules/aPollSlot/templates/_email_body.php
+++ modules/aPollSlot/templates/_email_body.php
@@ -18,3 +18,6 @@ echo a_('Poll "name" has a new submission from IP address ip at date', array(
 
 <p><?php echo a_('To administer the poll, click url', array('url' => link_to('here',url_for('@a_poll_poll_admin',array('absolute' => true))))); ?></p>
 
+<p><?php echo a_('To see the answer, click url', array('url' => link_to('here',url_for('@a_poll_answer_admin_show?id='.$answer->getId(),array('absolute' => true))))); ?></p>
+
+

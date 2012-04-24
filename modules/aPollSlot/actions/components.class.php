8465240e4c62e4c42a41e11a5f59f7d2835dfc67
Raffaele Bolliger <raffaele.bolliger@gmail.com>
2012-04-24 15:07:30 +0200

    Created base actions and componenets to allow extensions

--- modules/aPollSlot/actions/components.class.php
+++ modules/aPollSlot/actions/components.class.php
@@ -1,67 +1,7 @@
 <?php
 
-class aPollSlotComponents extends aSlotComponents {
+require_once dirname(__FILE__).'/../lib/BaseaPollSlotComponents.class.php';
 
-    public function executeEditView() {
-        // Must be at the start of both view components
-        $this->setup();
-
-        // Careful, don't clobber a form object provided to us with validation errors
-        // from an earlier pass
-        if (!isset($this->form)) {
-            $this->form = new aPollSlotEditForm($this->id, $this->slot->getArrayValue());
-        }
-    }
-
-    public function executeNormalView() {
-        $this->setup();
-
-        $this->poll = $this->slot->getPoll();
-        $type = $this->poll->getType();
-        
-        
-        if (!$this->show_poll = aPollToolkit::checkIfShowPoll($this->getRequest(), $this->poll)) {
-         
-            return 'Success';
-            
-        }
-        
-
-        // validating app_aPoll_available_polls entry
-        $this->poll_validation = aPollToolkit::checkPollConfiguration($type);
-
-
-        if ($this->poll_validation->isValid()) {
-
-
-
-            $form_name = aPollToolkit::getPollFormName($type);
-            $this->poll_form = new $form_name(array(
-                        'poll_id' => $this->poll->getId(),
-                        'slot_name' => $this->name,
-                        'permid' => $this->permid,
-                        'pageid' => $this->pageid,
-                        'remote_address' => $this->getRequest()->getRemoteAddress(),
-                        'culture' => $this->getUser()->getCulture(),
-                        'poll_type' => $type,
-                    ));
-            
-
-            // Getting view template partial to display the form.
-            // The template can be defined in two ways:
-            // - In app_aPoll_view_default_template, to set an overall template for all polls
-            // - In In app_aPoll_available_polls_XXX_view_template, where XXX 
-            //    is the name of this poll, to override the default display template
-            $this->form_view_template = aPollToolkit::getPollViewTemplate($type);
-
-
-            // Getting the action treating the form sumbission.
-            // The action can be defined in two ways:
-            // - In app_aPoll_view_submit_action, to set an overall action for all polls
-            // - In In app_aPoll_available_polls_XXX_submit_action, where XXX 
-            //    is the name of this poll, to override the default action
-            $this->submit_action = aPollToolkit::getPollSubmitAction($type);
-        }
-    }
-
+class aPollSlotComponents extends BaseaPollSlotComponents {
 }
+
---
+++ modules/aPollSlot/lib/BaseaPollSlotActions.class.php
@@ -0,0 +1,123 @@
+<?php
+
+class BaseaPollSlotActions extends aSlotActions {
+
+    public function executeEdit(sfRequest $request) {
+        $this->editSetup();
+
+        // Hyphen between slot and form to please our CSS
+        $value = $this->getRequestParameter('slot-form-' . $this->id);
+        $this->form = new aPollSlotEditForm($this->id, array());
+        $this->form->bind($value);
+        if ($this->form->isValid()) {
+            // Serializes all of the values returned by the form into the 'value' column of the slot.
+            // This is only one of many ways to save data in a slot. You can use custom columns,
+            // including foreign key relationships (see schema.yml), or save a single text value 
+            // directly in 'value'. serialize() and unserialize() are very useful here and much
+            // faster than extra columns
+
+            $values = $this->form->getValues();
+
+            // saving the reference to the poll
+            $this->slot->setPollId($values['poll']);
+
+            unset($values['poll']);
+
+            $this->slot->setArrayValue($values);
+
+            return $this->editSave();
+        } else {
+            // Makes $this->form available to the next iteration of the
+            // edit view so that validation errors can be seen, if any
+            return $this->editRetry();
+        }
+    }
+
+    public function executeSubmitPollForm(sfRequest $request) {
+
+        $values = $request->getParameter('a-poll-form');
+
+        $this->poll = Doctrine_Core::getTable('aPollPoll')->findOneById($values['poll_id']);
+        $this->forward404Unless($this->poll);
+
+        // validating app_aPoll_available_polls entry
+        $this->poll_validation = aPollToolkit::checkPollConfiguration($this->poll->getType());
+
+        // Creation of variables needed to render the partials
+        $this->page = aPageTable::retrieveByIdWithSlots($values['pageid']);
+
+        $this->slot = $this->page->getSlot(
+                $values['slot_name'], $values['permid']);
+
+        $type = $this->poll->getType();
+
+        $show_poll = aPollToolkit::checkIfShowPoll($request, $this->poll);
+
+
+        $form_name = aPollToolkit::getPollFormName($type);
+
+        // This is to allow parsing request parameters in order to add them to form inputs.
+        // This is useful for example to add recaptcha parameters, which are not defined directly
+        // in the main form.
+        $filtered = $this->getContext()->getEventDispatcher()->filter(
+                        new sfEvent($this, 'apoll.filter_submit_poll_request_parameters', array('poll_type' => $type)), $request->getParameterHolder()->getAll()
+                )->getReturnValue();
+
+
+        $values = array_merge($values, $filtered);
+
+
+        $this->poll_form = new $form_name(array(
+                    'poll_id' => $values['poll_id'],
+                    'slot_name' => $values['slot_name'],
+                    'permid' => $values['permid'],
+                    'pageid' => $values['pageid'],
+                    'remote_address' => $this->getRequest()->getRemoteAddress(),
+                    'culture' => $this->getUser()->getCulture(),
+                    'poll_type' => $type,
+                ));
+
+        $partial_vars = array(
+            "name" => $values['slot_name'],
+            "type" => 'aPoll',
+            "permid" => $values['permid'],
+            "pageid" => $values['pageid'],
+            "slot" => $this->slot,
+            "updating" => $this->updating,
+            "options" => $this->options,
+            "poll_validation" => $this->poll_validation,
+            "form_view_template" => aPollToolkit::getPollViewTemplate($type),
+            "submit_action" => aPollToolkit::getPollSubmitAction($type),
+            "poll" => $this->poll,
+            "poll_form" => $this->poll_form,
+            "show_poll" => $show_poll,
+        );
+
+
+        if ($this->poll_validation->isValid()) {
+
+            $this->poll_form->bind($values);
+
+            if ($this->poll_form->isValid()) {
+
+                $answer = $this->poll_form->save();
+
+                // sends a notification email
+                aPollToolkit::sendNotificationEmail($type, $this->getMailer(), $this->poll, $answer);
+
+                return $this->renderPartial($this->getModuleName() . '/submit_success', array_merge($partial_vars, array('template' => aPollToolkit::getPollSubmitSuccessTemplate($type))));
+            
+
+            } else {
+ 
+                return $this->renderPartial($this->getModuleName() . '/normalView', $partial_vars);
+            }
+        }
+    }
+
+    protected function getVariablesForSlot(sfRequest $request, aPollPoll $poll) {
+        
+    }
+
+}
+

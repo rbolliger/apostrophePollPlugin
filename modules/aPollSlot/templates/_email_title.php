bd1aa311ff30e2d22a643cbaf56b784a05320edb
Raffaele Bolliger <raffaele.bolliger@gmail.com>
2012-06-22 12:35:09 +0200

    - Improved code - Added french transmation to email messages

--- modules/aPollSlot/templates/_email_title.php
+++ modules/aPollSlot/templates/_email_title.php
@@ -1,3 +1,3 @@
 <?php use_helper('a'); ?>
 
-<?php echo a_('Poll "name": new user submission',array('name' => $poll->getTitle())); ?>
+<?php echo a_('Poll "%name%": new user submission',array('%name%' => $poll->getTitle())); ?>
